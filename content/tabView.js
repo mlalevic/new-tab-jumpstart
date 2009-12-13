04e357224006c287270a0469c81f64b417649ad1
mlalevic <mlalevic@223f7494-865d-11dd-8f23-d9c8888c2974>
2009-12-13 19:01:45 +0000

    Drag Drop feature Bookmarks toolbar feature
    Moved all logic from tile container binding to external js file
    Fixed search text box not to be so slim anymore

--- content/tabView.js
+++ content/tabView.js
@@ -22,6 +22,8 @@ if(!mlalevic){mlalevic = {};}
 if(!mlalevic.Utils){mlalevic.Utils = {};}
 if(!mlalevic.JumpStart){mlalevic.JumpStart = {};}
 
+var BookmarksEventHandler = null; //workaround for BookmarksEventHandler defined in browser.js
+
 (function(){
     var utils = mlalevic.Utils;
     var services = {}
@@ -123,6 +125,19 @@ if(!mlalevic.JumpStart){mlalevic.JumpStart = {};}
         drawThumbs();
         showClosed();
         showBookmarks();
+
+        //workaround for toolbar binding, XULBrowserWindow is not accessible from window, unless we do this "trick"
+        if(!window.XULBrowserWindow){
+          window.XULBrowserWindow = window.QueryInterface(Components.interfaces.nsIInterfaceRequestor)
+            .getInterface(Components.interfaces.nsIWebNavigation)
+            .QueryInterface(Components.interfaces.nsIDocShellTreeItem).treeOwner
+            .QueryInterface(Components.interfaces.nsIInterfaceRequestor)
+            .getInterface(Components.interfaces.nsIXULWindow)
+            .XULBrowserWindow;
+        }
+
+        //workaround for BookmarksEventHandler
+        BookmarksEventHandler = services.BrowserServices.getBookmarksEventHandler();
     }
 
     var getData = function(){
@@ -130,6 +145,43 @@ if(!mlalevic.JumpStart){mlalevic.JumpStart = {};}
             var result = properties.filter(function(element){return !element.removed;});
             result.sort(function(a,b){return a.index - b.index;});
             return result;
+    }
+
+    var moveTile = function(properties, newIndex){
+        var data = getData();
+        
+        //check and fix index
+        if(newIndex < 0){newIndex = 0;}
+        if(newIndex >= data.length) {newIndex = data.length - 1;}
+
+        var originalIndex = -1;
+        var originalProperties = null;
+        //find original index
+        for(var i = 0; i < data.length; i++){
+            if(properties.index == data[i].index){
+                originalIndex = i;
+                originalProperties = data[i];
+                break;
+            }
+        }
+
+        if(originalIndex == -1){return;}
+        if(originalIndex == newIndex){return;}
+
+        var insertIndex = newIndex;
+        /*if(originalIndex < newIndex){
+            insertIndex = newIndex - 1; //since we gonna remove from the list before the index
+        }*/
+
+        data.splice(originalIndex, 1);
+        data.splice(insertIndex, 0, originalProperties);
+
+        //renumber
+        for(var i = 0; i < data.length; i++){
+            data[i].index = i;
+        }
+        //update index properties
+        services.AnnoService.updateProperties(data);
     }
 
     var drawThumbs = function(){
@@ -158,7 +210,7 @@ if(!mlalevic.JumpStart){mlalevic.JumpStart = {};}
         //container.refreshThumb(properties);
       }
 
-      container.draw(data, Config.Thumbs, handleClick, handlePin, handleRemoved);
+      container.draw(data, Config.Thumbs, handleClick, handlePin, handleRemoved, moveTile);
     }
 
     var showClosed = function(){
@@ -276,6 +328,216 @@ if(!mlalevic.JumpStart){mlalevic.JumpStart = {};}
       // otherwise, we will end up calling load() twice
       historyTree.load([query], options);
     }
+
+    mlalevic.JumpStart.TileContainerController = {
+        view : null,
+        drawContext: null,
+        moveHandler: null,
+        setView : function(view){
+            this.view = view;
+        },
+        content : function(){
+          return this.view.getContent();
+        },
+        removeTiles: function(){
+            var rowContainer = this.content().childNodes[0].childNodes[1];
+            var rowsCount = rowContainer.childNodes.length;
+            for (var i = 0; i < rowsCount; i++) {
+              var row = rowContainer.childNodes[i];
+              var columnsCount = row.childNodes.length;
+              for (var j = 0; j < columnsCount; j++) {
+                var cell = row.childNodes[j];
+                while(cell.hasChildNodes()){
+                    cell.removeChild(cell.firstChild);
+                }
+              }
+            }
+        },
+        rearrange : function(indexFrom, indexTo){
+            if(!this.drawContext){
+                return;
+            }
+
+            //copy thumbs
+            var thumbs = [];
+            for(var i = 0; i < this.drawContext.data.length; i++){
+                thumbs.push(this.drawContext.data[i]);
+            }
+
+            //double check index
+            if(indexTo < 0){ indexTo = 0; }
+            if(indexTo >= thumbs.length){indexTo = thumbs.length - 1;}
+
+            //move thumb
+            var moved = thumbs[indexFrom];
+            thumbs.splice(indexFrom, 1);
+            thumbs.splice(indexTo, 0, moved);
+
+            var data = this.drawContext;
+            this.removeTiles();
+            this.drawTiles(thumbs, data.config, data.onClickHandler, data.pinHandler, data.removeHandler);
+        },
+        drawGrid : function(config){
+            columns = config.Columns;
+            rows = config.Lines;
+
+            // Create columns
+            var columnContainer = this.content().childNodes[0].childNodes[0];
+            for (var i = 0; i < columns; i++)
+              columnContainer.appendChild(document.createElement("column"));
+
+
+            // Create rows
+            var count = 0;
+            var rowContainer = this.content().childNodes[0].childNodes[1];
+            for (var i = 0; i < rows; i++) {
+
+              var row = document.createElement("row");
+              rowContainer.appendChild(row);
+              for (var j = 0; j < columns; j++) {
+                // Create tile container (drag-drop receiver)
+                var cell = document.createElement("box");
+                cell.setAttribute("class", config.ShowSmallThumbs?"smallTileCell": "tileCell");
+                cell.index = count;
+                var dragDropHandler = this.dragDropHandler;
+                cell.addEventListener("dragdrop", function(event){nsDragAndDrop.drop(event, dragDropHandler);}, false);
+                cell.addEventListener("dragover", function(event){nsDragAndDrop.dragOver(event, dragDropHandler);}, false);
+                cell.index = count;
+
+                row.appendChild(cell);
+
+                count ++;
+              }
+            }
+        },
+        draw : function(data, config, onClickHandler, pinHandler, removeHandler, moveHandler){
+                this.drawContext = {
+                    data: data,
+                    config: config,
+                    onClickHandler: onClickHandler,
+                    pinHandler: pinHandler,
+                    removeHandler: removeHandler,
+                };
+
+                this.moveHandler = moveHandler;
+
+                //TODO: (ML) create css class and add if data.length=0, that way we can ditch drawEmpty
+                if(data.length == 0){
+                    this.drawEmpty(config);
+                    return;
+                }
+
+                this.drawGrid(config);
+
+                this.drawTiles(data, config, onClickHandler, pinHandler, removeHandler);
+        },
+        drawTiles : function(data, config, onClickHandler, pinHandler, removeHandler){
+            function translate(data) {
+              let Cc = Components.classes;
+              let Ci = Components.interfaces;
+              var uri =          Cc["@mozilla.org/network/io-service;1"]
+                                     .getService(Ci.nsIIOService)
+                                     .newURI(data.url, null, null);
+              var annoSvc = Cc["@mozilla.org/browser/annotation-service;1"]
+                                     .getService(Ci.nsIAnnotationService);
+
+              var faviconUrl =   Cc["@mozilla.org/browser/favicon-service;1"]
+                                     .getService(Ci.nsIFaviconService)
+                                     .getFaviconImageForPage(uri).spec;
+              var thumbnailUrl;
+
+              if(annoSvc.pageHasAnnotation(uri, "jumpstart/thumbs")){
+                thumbnailUrl = annoSvc.getAnnotationURI(uri, "jumpstart/thumbs").spec;
+              }else{
+                thumbnailUrl = faviconUrl;
+              }
+
+              return {
+                original : data,
+                uri : uri,
+                host : data.host,
+                url : data.url,
+                title : data.title,
+                fav : faviconUrl,
+                originalUrl : data.originalUrl,
+                thumb : thumbnailUrl
+              }
+            }
+
+            var count = 0;
+            var rowContainer = this.content().childNodes[0].childNodes[1];
+            var rowsCount = rowContainer.childNodes.length;
+            for (var i = 0; i < rowsCount; i++) {
+              var row = rowContainer.childNodes[i];
+              var columnsCount = row.childNodes.length;
+              for (var j = 0; j < columnsCount; j++) {
+                var cell = row.childNodes[j];
+                if (count >= data.length){
+                  return;
+                }
+
+                // Create tile
+                var tile = document.createElement("vbox");
+                tile.setAttribute("class", config.ShowSmallThumbs?"smallthumb":"thumb");
+                cell.appendChild(tile);
+                tile.draw(config, translate(data[count]), onClickHandler);
+                tile.pinHandler = pinHandler;
+                tile.removeHandler = removeHandler;
+                tile.index = count;
+
+                count++;
+              }
+            }
+        },
+        drawEmpty : function(config){
+            this.content().childNodes[0].childNodes[2].hidden = false; //show label
+            this.drawGrid(config);
+        }
+    }
+
+
+    //drag
+    var controller = mlalevic.JumpStart.TileContainerController;
+    mlalevic.JumpStart.TileContainerController.dragDropHandler =  {
+          getSupportedFlavours : function () {
+            var flavours = new FlavourSet();
+            flavours.appendFlavour("application/jumpstart");
+            //flavours.appendFlavour("text/x-moz-url");
+
+            return flavours;
+          },
+          onDrop: function (event, transferData, session) {
+            if(!transferData.data){
+                return;
+            }
+            var data = utils.Converter.fromJSONString(transferData.data);
+            if(!data){
+                return;
+            }
+
+            controller.moveHandler(data, event.target.index);
+          },
+          onDragOver: function(event, flavour, session) {
+            var data = this.getData(flavour, session);
+            controller.rearrange(data.index, event.target.index);
+          },
+          getData: function(flavour, session){
+              var trans = Components.classes["@mozilla.org/widget/transferable;1"].
+                            createInstance(Components.interfaces.nsITransferable);
+              trans.addDataFlavor(flavour.contentType);
+              session.getData(trans, 0);
+
+              var str = { }, strlen = { };
+              trans.getTransferData(flavour.contentType, str, strlen);
+              if (!str.value)
+                return;
+
+              var strisupports = str.value.QueryInterface(Components.interfaces.nsISupportsString);
+
+              var jsonData = strisupports.data;
+              return utils.Converter.fromJSONString(jsonData);
+          }
+     }
 
 window.addEventListener("load", Show, false);
 window.addEventListener("unload", Unload, false);
